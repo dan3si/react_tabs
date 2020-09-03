6045563b7b2ab7aabd746fd3fca346bda0a4b36b
Dennis Priadka <dennis.priadka@gmail.com>
2020-09-03 14:19:43 +0300

    Updates

---
+++ precache-manifest.42943da4372565c1decb42b987a20f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd88f5cf07688942fb28889a4fda0fdb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a71c7d1665bb878dba44",
+    "url": "./static/css/main.50357cfc.chunk.css"
+  },
+  {
+    "revision": "c21235e66892e410b8ca",
+    "url": "./static/js/2.f6d830d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f6d830d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71c7d1665bb878dba44",
+    "url": "./static/js/main.703299c5.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
